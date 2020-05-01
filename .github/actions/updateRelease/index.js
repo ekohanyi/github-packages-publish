7892c26f08f4fc048bdeb11cba088d5057c640a6
Emma <emmajk2012@gmail.com>
2020-05-01 16:18:07 -0700

    get release by tag

--- .github/actions/updateRelease/index.js
+++ .github/actions/updateRelease/index.js
@@ -1,16 +1,20 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-// const getByTag = tag =>
-//   github.repos.getReleaseByTag({
-//     owner: this.context.repo.owner,
-//     repo: this.context.repo.repo,
-//     tag: tag
-//   });
+const getByTag = (git, tag) =>
+  git.GitHub.repos.getReleaseByTag({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    tag: tag
+  });
 
 try {
-  const rawTags = core.getInput("tags");
-  core.info(rawTags);
+  const tags = core.getInput("tags").split(",");
+  const token = core.getInput("token");
+  core.info(tags.toString());
+  const git = new github.GitHub(token);
+
+  core.info(JSON.stringify(getByTag(git, tags[0])));
 } catch (error) {
   core.error(error.toString());
   core.setFailed(error.change.toString());
