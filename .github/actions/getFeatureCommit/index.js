38af933965ec826337eb667f1bd288b75a8c96d8
Emma <emmajk2012@gmail.com>
2020-05-06 11:53:17 -0700

    action

---
+++ .github/actions/getFeatureCommit/index.js
@@ -0,0 +1,23 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+function getCommitHash() {
+  const changes = {};
+
+  switch (github.context.eventName) {
+    case "push": {
+      console.log(github.context);
+      return "0";
+    }
+    default: {
+      throw new Error(`Event "${github.context.eventName}" is not supported.`);
+    }
+  }
+}
+
+try {
+  core.setOutput("commit_hash", getCommitHash());
+} catch (error) {
+  core.error(error.toString());
+  core.setFailed(error.change.toString());
+}
