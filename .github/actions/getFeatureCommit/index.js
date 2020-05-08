4ed904df440056c68ccd80eafabb36e80e6cf1cd
Emma <emmajk2012@gmail.com>
2020-05-08 13:30:30 -0700

    tthrow merge error

--- .github/actions/getFeatureCommit/index.js
+++ .github/actions/getFeatureCommit/index.js
@@ -2,12 +2,17 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 function getCommitHash() {
-  const changes = {};
-
   switch (github.context.eventName) {
     case "push": {
-      console.log("git in action:", JSON.stringify(github));
-      return "0";
+      const length = github.context.payload.commits.length;
+      if (length > 1) {
+        // return second to last commit hash
+        return github.context.payload.commits[length - 2].id;
+      } else {
+        throw new Error(
+          `Commit does not come from a merged PR, no change log exists for it`
+        );
+      }
     }
     default: {
       throw new Error(`Event "${github.context.eventName}" is not supported.`);
